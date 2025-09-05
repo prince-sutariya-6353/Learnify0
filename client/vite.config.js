d1f8399834428c1e0ffb1cfa06f83aec56bc1851
prince <princegithub6353@gmail.com>
2025-09-05 10:14:52 +0530

    add vercel.json file

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
   plugins: [
-    tailwindcss(),
+    react(),        // React support
+    tailwindcss(),  // TailwindCSS support
   ],
-})
+  build: {
+    outDir: "dist", // Vercel expects static output in "dist"
+  },
+});
